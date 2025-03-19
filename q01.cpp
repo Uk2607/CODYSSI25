4001b50209b16219458039521f6969aec900933b
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-19 23:58:44 +0530

    files added

---
+++ q01.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+pair<vector<int>, string> get_input(string day) {
+    string file_path = "i"+day+".in";
+    ifstream file(file_path);
+
+    vector<int> arr;
+    string line, ops;
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << file_path << endl;
+        return {arr, ops};
+    }
+    while(getline(file, line)) {
+        if(line[0] == '+' || line[0] == '-') ops = line;
+        else arr.push_back(stoi(line));
+    }
+    file.close();
+    return {arr, ops};
+}
+
+void part1n2(vector<int>arr, string ops, int part=1) {
+    int x = arr[0];
+    for(int i=1;i<arr.size();i++) {
+        if(ops[i-1]=='+') x+=arr[i];
+        else x-=arr[i];
+    }
+    if(part == 1)
+        cout<<"Part1:: "<<x<<"\n";
+    else
+        cout<<"Part2:: "<<x<<"\n";
+}
+
+void part3(vector<int>arr, string ops) {
+    int x = arr[0]*10+arr[1];
+    for(int i=2;i<arr.size();i+=2) {
+        int y = arr[i]*10+arr[i+1];
+        if(ops[(i/2)-1]=='+') x+=y;
+        else x-=y;
+    }
+    cout<<"Part3:: "<<x<<"\n";
+}
+
+int main() {
+    vector<int>arr;
+    string ops;
+    pair<vector<int>, string>ip = get_input("01");
+    arr = ip.first;
+    ops = ip.second;
+    part1n2(arr, ops);
+    reverse(ops.begin(), ops.end());
+    part1n2(arr, ops, 2);
+    part3(arr, ops);
+}
+
+// Part 1: -196
+// Part 2: -60
+// Part 3: -645
---
+++ q02.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+const ll A = 151LL;
+const ll B = 85LL;
+
+
+vector<int> get_input(string day) {
+    string file_path = "/i"+day+".in";
+    ifstream file(file_path);
+
+    vector<int> arr;
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << file_path << endl;
+        return arr;
+    }
+    while(getline(file, line)) arr.push_back(stoi(line));
+    file.close();
+    return arr;
+}
+
+ll get_price(int n) {
+    // Function A: ADD 151
+    // Function B: MULTIPLY 85
+    // Function C: RAISE TO THE POWER OF 3
+    ll x = n;
+    return (x*x*x*B)+A;
+}
+
+void part1(vector<int>arr) {
+    sort(arr.begin(), arr.end());
+    int med = arr[arr.size()/2];
+    cout<<"Part1:: "<<get_price(med)<<"\n";
+}
+
+void part2(vector<int>arr) {
+    int sum = 0;
+    for(int x: arr) if(x%2==0) sum += x;
+    cout<<"Part2:: "<<get_price(sum)<<"\n";
+}
+
+void part3(vector<int>arr) {
+    ll limit = 15000000000000LL, diff = LLONG_MAX;
+    int room = -1;
+    for(int x: arr) {
+        ll price = get_price(x);
+        if(price<limit && limit-price<diff) {
+            diff = limit-price;
+            room = x;
+        }
+    }
+    cout<<"Part3:: "<<room<<"\n";
+}
+
+int main() {
+    vector<int>arr = get_input("02");
+    part1(arr);
+    part2(arr);
+    part3(arr);
+    return 0;
+}
+
+// Part 1: 11275335000151
+// Part 2: 2917782888825994711
+// Part 3: 5587
