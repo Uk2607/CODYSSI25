a2a552416c9c3133d3062437a4b68d344ae7fada
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-20 00:34:05 +0530

    d3p2

--- q03.cpp
+++ q03.cpp
@@ -8,11 +8,11 @@ using namespace std;
 #define ull unsigned long long
 
 
-vector<vector<int>> get_input(string day) {
+vector<vector<pair<int,int>>> get_input(string day) {
     string file_path = "ip/"+day+".in";
     ifstream file(file_path);
 
-    vector<vector<int>>arr;
+    vector<vector<pair<int,int>>>arr;
     string line;
 
     if (!file.is_open()) {
@@ -28,31 +28,38 @@ vector<vector<int>> get_input(string day) {
             string v;
             while(getline(ssv, v, '-')) row.push_back(stoi(v));
         }
-        arr.push_back(row);
+        arr.push_back({{row[0], row[1]}, {row[2], row[3]}});
     }
     file.close();
     return arr;
 }
 
-void part1(vector<vector<int>>arr) {
+void part1(vector<vector<pair<int,int>>>arr) {
     int boxes = 0;
-    for(vector<int>v:arr)
-        boxes += (v[1]-v[0]+1 + v[3]-v[2]+1);
+    for(vector<pair<int,int>>v:arr)
+        boxes += (v[0].second-v[0].first+1 + v[1].second-v[1].first+1);
     cout<<"Part1:: "<<boxes<<"\n";
 }
 
-void part2(vector<vector<int>>arr) {
-    cout<<"Part2:: "<<""<<"\n";
+void part2(vector<vector<pair<int,int>>>arr) {
+    int boxes = 0;
+    for(vector<pair<int,int>>v:arr) {
+        sort(v.begin(), v.end());
+        int l1 = v[0].first, r1 = v[0].second, l2 = v[1].first, r2 = v[1].second;
+        if(r1<l2) boxes += (r1-l1+r2-l2+2);
+        else boxes += (max(r2, r1)-l1+1);
+    }
+    cout<<"Part2:: "<<boxes<<"\n";
 }
 
-void part3(vector<vector<int>>arr) {
+void part3(vector<vector<pair<int,int>>>arr) {
     cout<<"Part3:: "<<""<<"\n";
 }
 
 int main() {
-    vector<vector<int>>arr = get_input("03");
+    vector<vector<pair<int,int>>>arr = get_input("03");
     part1(arr);
     part2(arr);
-    part3(arr);
+    // part3(arr);
     return 0;
 }
