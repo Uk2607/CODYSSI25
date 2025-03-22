f7ae0e85f2f83ecb9defc10eac71fb2b9b34dd7e
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-23 01:42:44 +0530

    d4p2

--- 2024/04.cpp
+++ 2024/04.cpp
@@ -3,6 +3,8 @@
 #include<sstream>
 #include<string>
 #include<set>
+#include<map>
+#include<queue>
 using namespace std;
 
 
@@ -37,9 +39,32 @@ void part1(vector<pair<string,string>>arr) {
 }
 
 void part2(vector<pair<string,string>>arr) {
-    string me = "STT";
-    int limit = 3;
-    cout<<"Part2:: "<<""<<"\n";
+    map<string, vector<string>>mp;
+    for(auto [src, dest]: arr) {
+        mp[src].push_back(dest);
+        mp[dest].push_back(src);
+    }
+    set<string>vis;
+    queue<string>q;
+
+    string start = "STT";
+    q.push(start);
+    int limit = 3, idx=0;
+
+    while(idx<=limit) {
+        int n = q.size();
+        while(n--) {
+            string curr = q.front();
+            q.pop();
+            vis.insert(curr);
+            for(string v: mp[curr]) {
+                if(vis.find(v)==vis.end())
+                    q.push(v);
+            }
+        }
+        idx++;
+    }
+    cout<<"Part2:: "<<vis.size()<<"\n";
 }
 
 void part3(vector<pair<string,string>>arr) {
