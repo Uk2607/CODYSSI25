6f000276e80d56747a7de4037fa8c5ccd05f2d6e
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-22 18:25:56 +0530

    2024 d1

--- 2024/01.cpp
+++ 2024/01.cpp
@@ -24,15 +24,26 @@ vector<int> get_input(string day) {
 }
 
 void part1(vector<int>arr) {
-    cout<<"Part1:: "<<""<<"\n";
+    int total = 0;
+    for(int x: arr) total += x;
+    cout<<"Part1:: "<<total<<"\n";
 }
 
 void part2(vector<int>arr) {
-    cout<<"Part2:: "<<""<<"\n";
+    sort(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.end());
+    int total = 0;
+    for(int i=20;i<arr.size();i++) total += arr[i];
+    cout<<"Part2:: "<<total<<"\n";
 }
 
 void part3(vector<int>arr) {
-    cout<<"Part3:: "<<""<<"\n";
+    int total = 0;
+    for(int i=0;i<arr.size();i++) {
+        if(i%2==0) total += arr[i];
+        else total -= arr[i];
+    }
+    cout<<"Part3:: "<<total<<"\n";
 }
 
 int main() {
@@ -42,3 +53,7 @@ int main() {
     part3(arr);
     return 0;
 }
+
+// Part 1: 153968069
+// Part 2: 134846540
+// Part 3: 1009247
