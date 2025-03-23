294cd82bfc09764f3d4269c0bb9b2d539eeb38ca
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-23 23:12:34 +0530

    d8 clear code

--- q08.cpp
+++ q08.cpp
@@ -8,54 +8,41 @@ using namespace std;
 #define ull unsigned long long
 
 
-pair<pair<vector<int>, vector<pair<int,int>>>,int> get_input(string day) {
+vector<int> get_input(string day) {
     string file_path = "ip/"+day+".in";
     ifstream file(file_path);
 
     vector<int>arr;
-    vector<pair<int,int>>ops;
-    int test;
     string line;
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
-        return {{arr, ops}, test};
+        return arr;
     }
-    int t=0;
     while(getline(file, line)) {
-        if(line.length()==0) {
-            t++;
-            continue;
-        }
-        if(t==0) arr.push_back(stoi(line));
-        else if(t==1) {
-            int idx;
-            for(int i=0;i<line.length();i++) if(line[i]=='-') {idx=i; break;}
-            ops.push_back({stoi(line.substr(0, idx)), stoi(line.substr(idx+1))});
-        }
-        else if(t==2) test = stoi(line);
+        arr.push_back(stoi(line));
     }
     file.close();
-    return {{arr, ops}, test};
+    return arr;
 }
 
-void part1(vector<int>arr, vector<pair<int,int>>ops, int test_idx) {
+void part1(vector<int>arr) {
     cout<<"Part1:: "<<""<<"\n";
 }
 
-void part2(vector<int>arr, vector<pair<int,int>>ops, int test_idx) {
+void part2(vector<int>arr) {
     cout<<"Part2:: "<<""<<"\n";
 }
 
-void part3(vector<int>arr, vector<pair<int,int>>ops, int test_idx) {
+void part3(vector<int>arr) {
     cout<<"Part3:: "<<""<<"\n";
 }
 
 int main() {
-    pair<pair<vector<int>, vector<pair<int,int>>>,int>ip = get_input("08");
-    part1(ip.first.first, ip.first.second, ip.second);
-    part2(ip.first.first, ip.first.second, ip.second);
-    part3(ip.first.first, ip.first.second, ip.second);
+    vector<int>arr = get_input("08");
+    part1(arr);
+    part2(arr);
+    part3(arr);
     return 0;
 }
 
